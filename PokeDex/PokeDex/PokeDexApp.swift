eabd4dc175afba5a8dedbb32daecfc51125a1cb5
Tariq Patel <tariqpatel9@gmail.com>
2024-02-20 20:12:46 +0200

    Project creation and Initial commit

---
+++ PokeDex/PokeDex/PokeDexApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokeDexApp.swift
+//  PokeDex
+//
+//  Created by Tasneem Patel on 2024/02/20.
+//
+
+import SwiftUI
+
+@main
+struct PokeDexApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
