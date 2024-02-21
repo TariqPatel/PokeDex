2b63c19165bb2951378b28c3f84b1e1f1a00a659
Tariq Patel <tariqpatel9@gmail.com>
2024-02-21 06:30:34 +0200

    Start adding pokemon images

--- PokeDex/PokeDex/Features/PokeList/PokeListView.swift
+++ PokeDex/PokeDex/Features/PokeList/PokeListView.swift
@@ -11,12 +11,36 @@ struct PokeListView: View {
     
     @StateObject var pokeListViewModel = PokeListViewModel()
     @State private var searchText = ""
+    let baseURLString = "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/1.png"
+    @Environment(\.displayScale) var scale
+        let size: CGFloat = 100
     
     var body: some View {
         NavigationStack {
             VStack {
                 List(searchResults, id: \.name) { pokemon in
-                    Text(pokemon.name)
+                    HStack {
+                        Text(pokemon.name.capitalized)
+                    Spacer()
+                    AsyncImage(url: URL(string: baseURLString),
+                                       scale: 3) { phase in
+                                switch phase {
+                                    case .empty:
+                                        ZStack {
+                                            Color.gray
+                                            ProgressView()
+                                        }
+                                    case .success(let image):
+                                        image.resizable()
+                                    case .failure(let error):
+                                        Text(error.localizedDescription)
+                                        // use placeholder for production app
+                                    @unknown default:
+                                        EmptyView()
+                                }
+                            }
+                            .frame(width: size, height: size)
+                    }
                 }
                 Button(action: {
                     pokeListViewModel.viewPokemon()
